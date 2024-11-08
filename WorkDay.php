0c0efd5d102c58b869e074a393e7e49abc58d96d
_m1ster_uzbek_ <jakbarov4401@gmail.com>
2024-11-08 23:04:53 +0500

    WorkDay.php

--- WorkDay.php
+++ WorkDay.php
@@ -28,21 +28,34 @@ class WorkDay{
         header('Location: index.php');
         return;
     }
+
     public function getWorDayList () {
         $query = "SELECT * FROM daily";
         $stmt = $this->conn->query($query);
         return $stmt->fetchAll();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     public function calculateDebtTimeForEachUser () {
         $query = "SELECT name, SUM(required_of) as debt FROM daily GROUP BY name";
-        $stmt = $this->conn->query($query);
-        return $stmt->fetchAll();
     }
     public function markAsDone (int $id) {
-        $query = "UPDATE daily SET required_of = 0 WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
         $stmt->execute();
         header('Location: index.php');
+    }
+    public function getWorkDayListWithPagination (int $offset) {
+        $offset = $offset ? ($offset * 10)-10 : 0;
+        $query = "SELECT * FROM daily ORDER BY arrived_at DESC LIMIT 10 OFFSET " . $offset;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function getTotalRecords () {
+        $query = "SELECT COUNT(id) as pageCount FROM daily";
+        $stmt = $this->conn->query($query);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    public function calculatePageCount () {
+        $total = $this->getTotalRecords()['pageCount'];
+        return ceil($total/10);
     }
 }
